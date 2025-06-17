198de709bfdc66de883207fa5eaefe59faa668a0
ambayyy9 <yksym00@gmail.com>
2025-06-17 21:08:36 +0800

    footer

--- action.php
+++ action.php
@@ -1,28 +1,25 @@
 <?php
 
 include "koneksi.php";
-$type = $_POST['simpan'];
-if ($type == "simpan") {
-   $member = $_POST['NAME'];
-    $email = $_POST['EMAIL'];
+if (isset($_POST['submit'])) {
+   $nm_member = $_POST['name'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
-    $tgl_lahir = $_POST['DATE'];
+    $tgl_lahir = $_POST['date'];
    
 
-    $input = "INSERT INTO tb_member (nm_member,email,password,tgl_lahir) VALUES ('$member','$email','$password','$tgl_lahir')";
+    $input = "INSERT INTO tb_member (nm_member, email, password, tgl_lahir) VALUES ('$nm_member','$email','$password','$tgl_lahir')";
     $query_input = mysqli_query($Open,$input);
-    if ($query_input) {
-        //Jika Sukses
-        ?>
-        <script language="JavaScript">
-            alert('Input Data Berhasil');
-            document.location = 'index.php';
-        </script>
-        <?php
+   if ($query_input) {
+    //Jika Berhasil
+        echo "<script>
+                alert('Input Data Berhasil');
+                window.location.href = 'index.php';
+              </script>";
 
     } else {
         //Jika Gagal
-        echo "Input Data Gagal!, Silahkan diulangi!";
+        echo "Input Data Gagal!, Silahkan diulangi! <br>" . mysqli_error($Open);
     }
 }
 

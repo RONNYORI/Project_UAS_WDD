c8bcf79f90baf406b721b636c52e66fe778ce968
ambayyy9 <yksym00@gmail.com>
2025-06-19 16:56:18 +0800

    ubahhhhh warnaa

--- login.php
+++ login.php
@@ -16,7 +16,7 @@
         <br><br>
 
         <div class="form">
-        <h3 align="center"> Create New Account Here</h3>
+        <h3 align="center"> Create New Account </h3>
         <h1></h1>
         
         <form action="action.php"  method="POST" name="form_input">
@@ -32,7 +32,7 @@
             <input id="password" type="password" name="password" placeholder=" make a strong password"/>
             <br />   
             <label for="date">DATE OF BIRTH</label><br />
-            <input id="date" type="date" name="date" value=" select "/>
+            <input id="date" type="date" name="date" value=" select " placeholder=""/>
             <br />         
             <br />
             <input id="submit" type="submit" value="Send" name="submit" href="#"/>
@@ -60,20 +60,35 @@
         <hr>
 
     <footer >
+
+<div class="footer-section about">
+    </div>
+
     <div class="socials">
+        <p>Follow the flavor trail</p> <br><br>
 	<a href="#">
 		<i data-feather="instagram"></i></a>
 	<a href="#">
 		<i data-feather="facebook"></i></a>
 	<a href="#">
 		<i data-feather="twitter"></i></a>
+    <a href="#">
+		<i data-feather="youtube"></i></a>
+    <a href="#">
+		<i class="fab fa-tiktok"></i></a>
    
     </div>
+    <script src="https://unpkg.com/feather-icons"></script>
+  <script>
+    feather.replace();
+  </script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
 
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126214.34859700906!2d115.14186952991787!3d-8.672674466806763!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2409b0e5e80db%3A0xe27334e8ccb9374a!2sDenpasar%2C%20Kota%20Denpasar%2C%20Bali!5e0!3m2!1sid!2sid!4v1730610558144!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
 		class="map"></iframe>
     
-        <p style="text-align: center;">Copyright &copy; 2025 Wander Taste.</p>
+        <p style="text-align: center;">Copyright &copy; 2025 Wander Taste. Semua Hak Dilindungi.</p>
 
     </footer>
 
